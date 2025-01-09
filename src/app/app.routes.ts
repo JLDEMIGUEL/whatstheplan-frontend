4bad9e5b2229da80c2eb6612f8868a6b093c44e6
jldemiguel <jldmp2001@gmail.com>
2025-01-09 13:58:00 +0100

    Added login and recommendations routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './modules/auth/register/register.component';
+import {LoginComponent} from './modules/auth/login/login.component';
+import {RecommendationsComponent} from './modules/recommendation/recommendations/recommendations.component';
 
 export const AppRoutes: Routes = [
   { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'recommendations', component: RecommendationsComponent },
   { path: '', redirectTo: '/recommendations', pathMatch: 'full' },
   { path: '**', redirectTo: '/recommendations' }
 ];
