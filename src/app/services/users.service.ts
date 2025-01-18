952e76730d28aee8d40d10b50aedd950de699ecc
jldemiguel <jldmp2001@gmail.com>
2025-01-18 18:39:58 +0100

    Added users.service.ts

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,57 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {fetchAuthSession} from 'aws-amplify/auth';
+import {Observable} from 'rxjs';
+import {switchMap} from 'rxjs/operators';
+import {environment} from '../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private baseUrl = environment.api;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getUser(): Observable<any> {
+    return this.addAuthHeaders().pipe(
+      switchMap((headers) => this.http.get(`${this.baseUrl}/users`, {headers}))
+    );
+  }
+
+  createUser(userData: any): Observable<any> {
+    return this.addAuthHeaders().pipe(
+      switchMap((headers) =>
+        this.http.post(`${this.baseUrl}/users`, userData, {headers})
+      )
+    );
+  }
+
+  updateUser(userData: any): Observable<any> {
+    return this.addAuthHeaders().pipe(
+      switchMap((headers) =>
+        this.http.put(`${this.baseUrl}/users`, userData, {headers})
+      )
+    );
+  }
+
+  private addAuthHeaders(): Observable<HttpHeaders> {
+    return new Observable((observer) => {
+      fetchAuthSession()
+        .then((session) => {
+          const token = session?.tokens?.accessToken?.toString();
+          const headers = new HttpHeaders({
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json'
+          });
+          observer.next(headers);
+          observer.complete();
+        })
+        .catch((error) => {
+          observer.error(error);
+        });
+    });
+  }
+}
