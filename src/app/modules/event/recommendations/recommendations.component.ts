6267c590d17b2c536070422beab8a1ca7c9fbe37
jldemiguel <jldmp2001@gmail.com>
2025-03-01 18:38:41 +0100

    Filters first version

--- src/app/modules/event/recommendations/recommendations.component.ts
+++ src/app/modules/event/recommendations/recommendations.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {NgForOf, NgIf} from '@angular/common';
+import {CommonModule, NgForOf, NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 import {Router} from '@angular/router';
 import {HttpErrorResponse} from '@angular/common/http';
 import {EventsService} from '../../../services/events.service';
@@ -11,7 +12,9 @@ import {environment} from '../../../../environments/environment';
   standalone: true,
   imports: [
     NgIf,
-    NgForOf
+    NgForOf,
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './recommendations.component.html',
   styleUrls: ['./recommendations.component.scss']
@@ -21,6 +24,35 @@ export class RecommendationsComponent implements OnInit {
   recommendations: WTPEvent[] = [];
   errorMessage!: string;
   isLoading: boolean = true;
+
+  // Filters object
+  filters = {
+    location: '',
+    durationFrom: '',
+    durationTo: '',
+    dateTimeFrom: '',
+    dateTimeTo: '',
+    activityTypes: [] as string[]
+  };
+
+  // List of available cities for the location dropdown
+  availableCities: string[] = [
+    'Madrid',
+    'Barcelona',
+    'Sevilla',
+    'Valencia',
+    'Bilbao',
+    'London',
+    'Paris',
+    'Berlin'
+  ];
+
+  // Example list of activity types
+  availableActivityTypes: string[] = ['Music', 'Sports', 'Arts', 'Technology'];
+
+  // Dropdown toggles for range pickers
+  durationDropdownOpen: boolean = false;
+  dateTimeDropdownOpen: boolean = false;
 
   constructor(private eventsService: EventsService, private router: Router) {
   }
@@ -57,5 +89,33 @@ export class RecommendationsComponent implements OnInit {
 
   async createNewEvent(): Promise<void> {
     await this.router.navigate(['/events-create']);
+  }
+
+  // Method to toggle the duration dropdown
+  toggleDurationDropdown(): void {
+    this.dateTimeDropdownOpen = false;
+    this.durationDropdownOpen = !this.durationDropdownOpen;
+  }
+
+  // Close duration dropdown (e.g., after selection)
+  closeDurationDropdown(): void {
+    this.durationDropdownOpen = false;
+  }
+
+  // Method to toggle the datetime dropdown
+  toggleDateTimeDropdown(): void {
+    this.durationDropdownOpen = false;
+    this.dateTimeDropdownOpen = !this.dateTimeDropdownOpen;
+  }
+
+  // Close datetime dropdown (e.g., after selection)
+  closeDateTimeDropdown(): void {
+    this.dateTimeDropdownOpen = false;
+  }
+
+  // Stub for applying filters
+  applyFilters(): void {
+    console.log('Applied filters:', this.filters);
+    // TODO: Call service to query events with these filters.
   }
 }
